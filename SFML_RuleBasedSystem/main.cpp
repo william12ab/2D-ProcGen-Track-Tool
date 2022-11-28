3f0ecdf93e4d527440b824cf17c1090412cbfd4f
william12ab <58400045+william12ab@users.noreply.github.com>
2022-11-28 13:52:52 +0000

    works for multiple

--- SFML_RuleBasedSystem/main.cpp
+++ SFML_RuleBasedSystem/main.cpp
@@ -174,7 +174,7 @@ int main()
 			ImGui::SliderInt("Size of cell outlines", &number_, 0, 100);
 			ImGui::Text("lower = large outlines");
 			ImGui::Text("higher = less effect");
-			ImGui::SliderInt("Number of Layers of Noise", &layers_,0, 10);
+			ImGui::SliderInt("Number of Layers", &layers_,1, 10);
 			ImGui::SliderInt("Alpha", &alpha_, 0, 255);
 			ImGui::SliderInt("Radius Cut-off:", &radius_cutoff, 50, 255);
 			ImGui::Text("lower = larger radius");
@@ -300,12 +300,13 @@ int main()
 			if (ImGui::Button("Renerate (Noise Method)"))
 			{
 				ClearConsoleWin();
-				v_d_p->vector_all(peaks_to_count_+1);
-				for (int i = 0; i < peaks_to_count_; i++)
+				v_d_p->vector_all(peaks_to_count_*2);
+				for (int i = 0; i < (peaks_to_count_*2); i++)
 				{
 					v_d_p->FindMax(v_d_p->GetGridSize(), layers_, i_p_p->GetNoiseMap());								//finds the highest point in the terrain
 					v_d_p->DirectionDecider(v_d_p->GetGridSize(), radius_cutoff, layers_, i, i_p_p->GetNoiseMap(),v_d_p->GetHighPoint(),true);		//finds point on circumference 
 					v_d_p->DirectionDecider(v_d_p->GetGridSize(), 85, layers_, i+1, i_p_p->GetNoiseMap(), v_d_p->GetLowPoint(),false);		//finds point on circumference 
+					i++;		//because the iterator changes above right here |
 				}
 				t_t_p->TerrainLoop(v_d_p, s_p_p,voronoi_d,height_map,n_height_map,i_p_p,number_,track_type_);
 			}
